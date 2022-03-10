1ce20caa5047905beaf369d0dd84de83700ac3fd
valik <valik324453@mail.ru>
2022-03-10 18:34:38 +0200

    Create login form

---
+++ WpfStarter/WpfStarter/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfStarter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Button button = new Button();
+            //button.Width = 100;
+            //button.Height = 30;
+            //button.Content = "Custom Btn";
+            //button.Background = new SolidColorBrush(Colors.Red);
+
+            //myGrid.Children.Add(button);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Welcome to Wpf lesson One !!!");
+            /*string tbContent = textBox1.Text;
+            if (tbContent != "")
+            {
+                MessageBox.Show(tbContent);
+            }
+            else
+            {
+                MessageBox.Show("Input is empty");
+            }*/
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("SDFsdfdsf");
+        }
+    }
+}
---
+++ WpfStarter/WpfStarter/Notebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfStarter
+{
+    public class Notebook
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Notebook {Name}; Price: {Price}";
+        }
+    }
+}
